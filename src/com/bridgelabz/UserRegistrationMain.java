a69f9e13b0c3b086e7c2b64a8ff4707a49bd06c7
namdevchavanmnb <namdevchavanmnb@gmail.com>
2022-11-07 06:58:07 +0530

    Welcome

---
+++ src/com/bridgelabz/UserRegistrationMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+import com.sun.tools.javac.Main;
+
+public class UserRegistrationMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+    }
+}
